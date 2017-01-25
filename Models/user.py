56288da4140861821ae238c48fc6f3bc29782dee
michaland <michal2302@gmail.com>
2017-01-25 10:43:58 +0100

    User class created, Student and Mentor classes moved to user file

--- Models/user.py
+++ Models/user.py
@@ -1,3 +1,11 @@
+class User:
+
+    def __init__ (self, name, mail, password):
+        self.name = name
+        self.mail = mail
+        self.password = password
+
+
 class Student:
 
     student_list = []
@@ -60,3 +68,30 @@ class Student:
                         print('there is no such submission added by {}'.format(student.name))
             else:
                 print('there is no such student in students list')
+
+
+class Mentor:
+
+    mentor_list = []
+
+    def __init__(self, name, mail, password):
+        self.name = name
+        self.mail = mail
+        self.password = password
+
+    @classmethod
+    def add_mentor(cls, mentor):
+        cls.mentor_list.append(mentor)
+
+    def edit_mentor(self, name, mail, password):
+        self.name = name
+        self.mail = mail
+        self.password = password
+
+    @classmethod
+    def remove_mentor(cls, mentor):
+        cls.mentor_list.remove(mentor)
+
+    @classmethod
+    def get_list_mentor(cls):
+        return cls.mentor_list
