386ed567619553130a011187a1add307d04891a7
michaland <michal2302@gmail.com>
2017-01-25 12:07:47 +0100

    ui.py created

---
+++ ui.py
@@ -0,0 +1,8 @@
+class UserInterface:
+
+    @staticmethod
+    def login:
+        user_name = input('Provide your username: ')
+        user_password = input('Provide your password: ')
+        return (user_name, user_password)
+    
