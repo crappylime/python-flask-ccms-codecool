1c51a74badaa0975e78ae4d652d38cf17b31e8eb
Marek Frankowicz <frankowicz.marek@gmail.com>
2017-01-25 21:57:31 +0100

    Add *args to read tuples

--- menu.py
+++ menu.py
@@ -41,7 +41,7 @@ class StudentMenu:
         while True:
             user_choice = UserInterface.student_menu()
             if user_choice == "Submit an assignment":
-                Submission.add_submission(UserInterface.get_submit_data())
+                Submission.add_submission(*UserInterface.get_submit_data())
             elif user_choice == "View my grades":
                 UserInterface.view_grade(user)
             elif user_choice == "Log out":
@@ -55,15 +55,15 @@ class MentorMenu:
             if user_choice == "Show students list":
                 UserInterface.show_list(Student.get_student_list())
             elif user_choice == "Add an assignment":
-                Assignment.add_assignment(UserInterface.get_assignment_data())
+                Assignment.add_assignment(*UserInterface.get_assignment_data())
             elif user_choice == "Grade an assignment":
-                Assignment.set_grade_submission(UserInterface.get_grade_assignment_data())
+                Assignment.set_grade_submission(*UserInterface.get_grade_assignment_data())
             elif user_choice == "Check attendance":
                 pass
             elif user_choice == "Add student":
-                Student.add_student(UserInterface.get_user_data())
+                Student.add_student(*UserInterface.get_user_data())
             elif user_choice == "Remove student":
-                pass
+                Student.remove_student(UserInterface.get_remove_data())
             elif user_choice == "Edit student data":
                 pass
             elif user_choice == "Log out":
