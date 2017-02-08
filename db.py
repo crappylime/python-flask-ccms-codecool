0d6e61ad9f23a4961a3f0198b7b33c53da7ad8ed
Marta Sajdak <crappylime@op.pl>
2017-02-08 15:40:02 +0100

    Extract executing queries, add deleting dependent records

--- db.py
+++ db.py
@@ -5,25 +5,56 @@ class DB:
 
     @classmethod
     def connect(cls):
-        conn = sqlite3.connect('Data/ccms.db')
-        return conn.cursor()
+        return sqlite3.connect('Data/ccms.db')
 
     @classmethod
-    def delete_assignment_record(cls):
-        cur = cls.connect()
-        cur.execute("DELETE FROM assignments WHERE assignment_id = ?", assignment_id)
+    def execute_query(cls, query, args):
+        conn = cls.connect()
+        cur = conn.cursor()
+        if type(args) is tuple:
+            args = [args]
+        cur.executemany(query, args)
+        conn.commit()
+        conn.close()
 
     @classmethod
-    def delete_attendance_record(cls):
-        cur = cls.connect()
-        cur.execute("DELETE FROM attendances WHERE attendance_id = ?", attendance_id)
+    def delete_assignment_record(cls, assignment_id):
+        query = "DELETE FROM assignments WHERE assignment_id = ?"
+        args = assignment_id
+        cls.execute_query(query, (args,))
 
     @classmethod
-    def delete_submission_record(cls):
-        cur = cls.connect()
-        cur.execute("DELETE FROM submissions WHERE submission_id = ?", submission_id)
+    def delete_attendance_record(cls, attendance_id):
+        query = "DELETE FROM attendances WHERE attendance_id = ?"
+        args = attendance_id
+        cls.execute_query(query, (args,))
 
     @classmethod
-    def delete_user_record(cls):
-        cur = cls.connect()
-        cur.execute("DELETE FROM users WHERE user_id = ?", user_id)
+    def delete_submission_record(cls, submission_id):
+        query = "DELETE FROM submissions WHERE submission_id = ?"
+        args = submission_id
+        cls.execute_query(query, (args,))
+
+    @classmethod
+    def delete_user_record(cls, user_id):
+        query = "DELETE FROM users WHERE user_id = ?"
+        args = user_id
+        cls.execute_query(query, (args,))
+
+    @classmethod
+    def delete_user_attendance_record(cls, user_id):
+        query = "DELETE FROM attendances WHERE user_id = ?"
+        args = user_id
+        cls.execute_query(query, (args,))
+
+    @classmethod
+    def delete_user_submission_record(cls, user_id):
+        query = "DELETE FROM submissions WHERE user_id = ?"
+        args = user_id
+        cls.execute_query(query, (args,))
+
+    @classmethod
+    def delete_assignment_submission_record(cls, assignment_id):
+        query = "DELETE FROM submissions WHERE assignment_id = ?"
+        args = assignment_id
+        cls.execute_query(query, (args,))
