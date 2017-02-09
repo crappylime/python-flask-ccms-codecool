c2ce3311f7ea3c5dc32fb587c108225b88567481
Marek Frankowicz <frankowicz.marek@gmail.com>
2017-02-09 09:59:12 +0100

    Add read_user_record_by_user_id method in DB class

--- db.py
+++ db.py
@@ -7,3 +7,12 @@ class DB:
     def connect(cls):
         return sqlite3.connect('ccms.db')
 
+    @classmethod
+    def read_user_record_by_user_id(cls, user_id):
+        conn = cls.connect()
+        cursor = conn.cursor()
+        query = "SELECT * FROM `users` WHERE `user_id` = ?;"
+        cursor.execute(query, (user_id,))
+        user = cursor.fetchall()
+        conn.close()
+        return user
