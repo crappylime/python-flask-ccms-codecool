f97f115ed20b74c0fae23f90c0e741b90a00c219
Marek Frankowicz <frankowicz.marek@gmail.com>
2017-02-23 14:17:58 +0100

    Add js login validation, add error messages to index.html and to style.css

---
+++ Views/js/login.js
@@ -0,0 +1,22 @@
+function validate(event) {
+    event.preventDefault();
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))
+    {
+        document.getElementById("email_format_error").style.display = 'block';
+        return false;
+    } else {
+        document.getElementById("email_length_error").style.display = 'none';
+    }
+    if (password.length < 5) {
+        document.getElementById("password_error").style.display = 'block';
+        return false;
+    }
+    else {
+        window.location = "user_details.html";
+        console.log("after");
+        return true;
+    }
+}
+
