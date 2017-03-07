e6b0e9edddd6e92c5be8ec41da71804d31e9f5c5
Marek Frankowicz <frankowicz.marek@gmail.com>
2017-03-07 14:35:16 +0100

    Add new assignment GET and POST to assignment_ctrl and add_edit_assignment.html, modify title of assignment_detail.html, unlock urls in layout.html

--- controllers/assignments_ctrl.py
+++ controllers/assignments_ctrl.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect
 from flask import Blueprint
 from models.assignments import Assignment
 
@@ -13,3 +13,20 @@ def assignments():
 @assignments_ctrl.route("/assignments/<assignment_id>")
 def assignment_details(assignment_id):
     return render_template("assignment_details.html", assignment=Assignment.get_assignment_by_id(assignment_id))
+
+
+@assignments_ctrl.route("/assignments/new", methods=["GET", "POST"])
+def assignment_new():
+    if request.method == "POST":
+        assignment_title = request.form['assignment_title']
+        content = request.form['content']
+        due_date = request.form['due_date']
+        max_points = request.form['max_points']
+        Assignment.add_assignment(assignment_title, content, due_date, max_points)
+        return redirect('/assignments')
+    return render_template("add_edit_assignment.html", title="Add an assignment")
+
+
+# @assignments_ctrl.route("/assignments/new")
+# def assignment_new():
+#     return render_template("add_edit_assignment.html", title="Add an assignment")
