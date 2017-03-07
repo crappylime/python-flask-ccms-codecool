07f9c175ee59b023fae6f262f3f3bdc72a9e40d9
Marta Sajdak <crappylime@op.pl>
2017-03-07 15:15:11 +0100

    Deploy listing assignment submissions
    handle situation with no submissions
    redefine transmission of assignment object

--- controllers/submissions_ctrl.py
+++ controllers/submissions_ctrl.py
@@ -1,11 +1,14 @@
 from models.submissions import Submission
+from models.assignments import Assignment
 from flask import Blueprint, render_template
 
 submissions_ctrl = Blueprint('submissions_ctrl', __name__)
 
 
-@submissions_ctrl.route("/<assignment_id>/submissions")
-def list_assignment_submissions(assignment_id, assignment):
+@submissions_ctrl.route("/assignments/<assignment_id>/submissions")
+def list_assignment_submissions(assignment_id):
     """ Shows list of submissions stored in the database.
     """
-    return render_template('submissions.html', assignment,  list_assignment_submissions=Submission.get_submission_list_by_assignment_id(assignment_id))
+    assignment = Assignment.get_assignment_by_id(assignment_id)
+    assignment_id = assignment.get_id()
+    return render_template('submissions.html', assignment=assignment,  list_assignment_submissions=Submission.get_submission_list_by_assignment_id(assignment_id))
