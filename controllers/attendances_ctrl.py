6e7f0d2ce255e2c561d709270ad02498b8968614
michaland <michal2302@gmail.com>
2017-03-07 18:45:19 +0100

    Done check attendance (without date)

--- controllers/attendances_ctrl.py
+++ controllers/attendances_ctrl.py
@@ -17,9 +17,19 @@ def attendances():
 
 
 @attendances_ctrl.route("/attendances/check/", methods=['GET', 'POST'])
-def check_attendamce():
-
+def check_attendance():
     students_list = User.get_user_list_by_role('student')
+
+    if request.method == 'POST':
+        # return 'oj'
+        attendances_list = []
+        for student in students_list:
+            attendances_list.append((student.get_id(), "2030-02-09", request.form[str(student.get_id())]))
+        print(attendances_list)
+        for atten in attendances_list:
+            Attendance.add_attendance(atten[0], atten[1], atten[2])
+
+
 
     return render_template('attendance_check.html', students_list=students_list)
 
