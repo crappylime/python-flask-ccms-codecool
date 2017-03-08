6aa47055ef1777e9c77de2908fd2a1771249e3b2
michaland <michal2302@gmail.com>
2017-03-08 14:04:32 +0100

    Add show attendance list by date

--- controllers/attendances_ctrl.py
+++ controllers/attendances_ctrl.py
@@ -2,20 +2,23 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from models.attendances import Attendance
 from models.users import User
 from flask import Blueprint
-
+import time
 
 
 attendances_ctrl = Blueprint('attendances_ctrl', __name__)
 
 
-@attendances_ctrl.route("/attendances")
+@attendances_ctrl.route("/attendances", methods=['GET', 'POST'])
 def attendances():
 
-    attendances_list = Attendance.get_attendance_list()
+    chosen_date = request.args.get('date', None)
 
-    name_dict = {1: 'Present', 0: 'Absent', 0.8: 'Late' }
+    if chosen_date:
+        attendances_list = Attendance.get_attendance_list_by_date(chosen_date)
+    else:
+        attendances_list = Attendance.get_attendance_list()
 
-    return render_template('attendance_list.html', attendances_list=attendances_list)
+    return render_template('attendance_list.html', attendances_list=attendances_list, chosen_date=chosen_date)
 
 
 @attendances_ctrl.route("/attendances/check/", methods=['GET', 'POST'])
@@ -30,7 +33,11 @@ def check_attendance():
         student_status_dict[attendance.get_student().get_id()] = attendance.get_status()
 
     if request.method == 'GET':
-        if chosen_date is None or len(by_date_list) == 0:
+        if chosen_date is None:
+            chosen_date = time.strftime("%Y-%m-%d")
+            return render_template('attendance_check.html', students_list=students_list,
+                                   chosen_date=chosen_date, student_status_dict={})
+        elif len(by_date_list) == 0:
             return render_template('attendance_check.html', students_list=students_list,
                                    chosen_date=chosen_date, student_status_dict={})
         else:
@@ -38,6 +45,9 @@ def check_attendance():
                                    chosen_date=chosen_date, student_status_dict=student_status_dict)
 
     if request.method == 'POST':
+        if chosen_date is None:
+            chosen_date = time.strftime("%Y-%m-%d")
+            by_date_list = Attendance.get_attendance_list_by_date(chosen_date)
         attendances_list = []
         if len(by_date_list) == 0:
             for student in students_list:
@@ -53,6 +63,3 @@ def check_attendance():
                 Attendance.update_attendance(attendance[0], attendance[1], attendance[2])
             return render_template('attendance_check.html', students_list=students_list,
                                    student_status_dict={})
-
-    # return render_template('attendance_check.html', students_list=students_list,
-    #                        chosen_date=chosen_date, student_status_dict={})
