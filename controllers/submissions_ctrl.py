54c6527449233671465bf422c2292501af06f14a
Marta Sajdak <crappylime@op.pl>
2017-03-08 12:06:25 +0100

    Implement adding new submission

--- controllers/submissions_ctrl.py
+++ controllers/submissions_ctrl.py
@@ -1,6 +1,6 @@
 from models.submissions import Submission
 from models.assignments import Assignment
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for, request, redirect
 
 submissions_ctrl = Blueprint('submissions_ctrl', __name__)
 
@@ -20,3 +20,17 @@ def submission_details(submission_id):
     """
     submission = Submission.get_submission_by_id(submission_id)
     return render_template('submission_details.html', submission=submission, assignment=submission.get_assignment())
+
+
+@submissions_ctrl.route('/assignments/<assignment_id>/submissions/new', methods=['GET', 'POST'])
+def submission_add(assignment_id):
+    """ Creates new submission
+    If the method was GET it should show new submission form.
+    If the method was POST it should create, save new submission.
+    """
+    student_id = 2
+    assignment = Assignment.get_assignment_by_id(assignment_id)
+    if request.method == 'POST':
+        Submission.add_submission(assignment_id, student_id, request.form['content'])
+        return redirect(url_for('assignments_ctrl.assignments'))
+    return render_template('add_submission.html', assignment=assignment)
