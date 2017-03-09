ee905ef84dc19c305378ad1e99c5e1d45028b75e
michaland <michal2302@gmail.com>
2017-03-09 14:03:47 +0100

    Implement displaying overall attendance by date, fix typos

--- db_controller.py
+++ db_controller.py
@@ -294,6 +294,19 @@ class DB:
         return overall_attendance
 
     @classmethod
+    def read_overall_attendance_by_date(cls, date):
+        """Read overall attendance by provided student id"""
+        conn = cls.connect()
+        cursor = conn.cursor()
+        query = "SELECT round(avg(100.0*status), 2) from attendances WHERE `date` = ?;"
+        args = (date,)
+        cursor.execute(query, args)
+        overall_attendance = cursor.fetchall()[0][0]
+        conn.close()
+        return overall_attendance
+
+
+    @classmethod
     def read_team_list(cls):
         """Read team record list"""
         conn = cls.connect()
