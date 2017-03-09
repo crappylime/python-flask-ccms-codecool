f1c6089f8eef975de6b508b343655678834b97c2
totine <joanna.gargas@gmail.com>
2017-03-09 12:41:16 +0100

    Add experimental menu classes

--- models/menus.py
+++ models/menus.py
@@ -1,5 +1,10 @@
-# class MainMenu:
-#
-#     def __init__(self, name):
-#         self
-#         self.submenus
+class MainMenu:
+
+    def __init__(self, name, permissions):
+        self.name = name
+        self.submenus = []
+        self.permissions = permissions
+
+class SubMenu:
+    def __init__(self, name, url_for, url_for_args, permisions, mainmenu):
+       pass
