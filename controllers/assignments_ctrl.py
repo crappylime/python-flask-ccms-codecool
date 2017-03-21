69c01fb8d647c53ea2b8247e3b6251fd5bf5a520
Michał Kaczkowski <michal2302@gmail.com>
2017-03-21 22:58:26 +0100

    Add editing assignment from filled modal window by AJAX - without AJAX updating table yet

--- controllers/assignments_ctrl.py
+++ controllers/assignments_ctrl.py
@@ -3,6 +3,7 @@ from flask import Blueprint
 from models.assignments import Assignment
 from models.menus import Menu
 from db_controller import DB
+import json
 
 
 assignments_ctrl = Blueprint('assignments_ctrl', __name__)
@@ -34,6 +35,10 @@ def assignment_new():
         return redirect(url_for('assignments_ctrl.assignments'))
     return render_template("add_edit_assignment.html", title="Add an assignment", mainmenu=mainmenu)
 
+# ----------------------------------------------------------------------
+# ----------------------- routes for AJAX: -----------------------------
+# ----------------------------------------------------------------------
+
 
 @assignments_ctrl.route("/new_assignment", methods=["GET", "POST"])
 def new_assignment():
@@ -49,6 +54,43 @@ def new_assignment():
 
     Assignment.add_assignment(assignment_title, is_team, content, due_date, max_points)
     return 'added'
+
+
+@assignments_ctrl.route("/get_assignment_by_id", methods=["POST"])
+def get_assignment_by_id():
+
+    print('im in get_assignment_by_id')
+
+    assignment_id = request.get_json()
+
+    assignment = Assignment.get_assignment_by_id(assignment_id)
+    assignment_in_json = json.dumps(assignment.__dict__, ensure_ascii=False).encode('utf8')
+    print(assignment_in_json)
+
+    return assignment_in_json
+
+
+@assignments_ctrl.route("/edit_assignment", methods=["POST"])
+def edit_assignment():
+
+    print('im in flask edit_assignment')
+
+    assignment_content = request.get_json()
+    print(assignment_content)
+    assignment_title = assignment_content["assignment_title"]
+    content = assignment_content["content"]
+    due_date = assignment_content["due_date"]
+    max_points = assignment_content["max_points"]
+    is_team = assignment_content["is_team"]
+    assignment_id = assignment_content["id"]
+
+    Assignment.get_assignment_by_id(assignment_id).edit_assignment(assignment_title, is_team, content, due_date,
+                                                                   max_points)
+    return 'edited'
+
+# ----------------------------------------------------------------------
+# ----------------------------------------------------------------------
+
 
 @assignments_ctrl.route("/assignments/<assignment_id>/edit", methods=["GET", "POST"])
 def assignment_edit(assignment_id):
--- static/js/common.js
+++ static/js/common.js
@@ -20,11 +20,123 @@ $(document).ready(function() {
 });
 
     // SHOW MODAL
-function showModalAdd() {
+function showModalAdd(id) {
     var modalAdd = document.getElementById('modalAdd');
-    modalAdd.style.display = "block";
-}
 
+    if (id == 'new_assignment_empty_form') {
+        // alert('empty form please');
+        modalAdd.style.display = "block";
+    }
+    else {
+         var id_in_JSON = JSON.stringify(id);
+         $.ajax({
+            type: 'POST',
+            url: '/get_assignment_by_id',
+            contentType: 'application/json',
+            data: id_in_JSON,
+            success: function(assignment_json_object) {
+
+                var JS_object = JSON.parse(assignment_json_object);
+
+                $('#assignment_title').val(JS_object.title);
+                $('#due_date').val(JS_object.due_date);
+                $('#max_points').val(JS_object.max_points);
+                $('#content').val(JS_object.content);
+                if (JS_object.is_team == 0) {
+                    $("#is_team").prop('checked', false);
+                }
+                else {$("#is_team").prop('checked', true);}
+                $("#add_button").attr("onclick","add_new_or_edit_assignment('edit_assignment'," + JS_object.id + ")"); // construct custom link
+                modalAdd.style.display = "block";
+            },
+            error: function () {
+                alert('error filling edited assignment fields')
+            }
+    });
+
+    modalAdd.style.display = "block";
+}};
+
+
+
+
+
+//-------------------------------------------------------------------------------------//
+//                              MODAL'S ADD ASSIGNMENT
+//-------------------------------------------------------------------------------------//
+
+
+function add_new_or_edit_assignment(choice, id) {
+
+        var modalAdd = document.getElementById('modalAdd');
+
+        if (choice == 'new_assignment') {
+            var path = '/new_assignment';
+        }
+        else {
+            var path = '/edit_assignment';
+        }
+
+        // --------------- get values from form -------------------- //
+        var assignment_title = $('#assignment_title');
+        var is_team = $("#is_team");
+        var due_date = $('#due_date');
+        var max_points = $('#max_points');
+        var content = $('#content');
+
+        if (is_team.prop('checked') == false) {
+            is_team = 0}
+        else {is_team = 1}
+
+        var new_assignment = {
+            assignment_title: assignment_title.val(),
+            is_team: is_team,
+            max_points: max_points.val(),
+            due_date: due_date.val(),
+            content: content.val(),
+            id: id      // necessary when we want to edit assignment / automatically excluded if we add new assignment
+        };
+        var JSON_new_assignment = JSON.stringify(new_assignment);  // convert JS object to JSON string
+
+        // --------- send values to controller by AJAX: ------------- //
+        if (choice == 'new_assignment') {
+        $.ajax({
+            type: 'POST',
+            url: '/new_assignment',
+            contentType: 'application/json',
+            data: JSON_new_assignment,
+            success: function() {         // response_data = sorted to-do list in JSON format
+                modalAdd.style.display = "none";
+                $('#assignment_title').val(undefined);
+                $("#is_team").prop('checked', false);
+                $('#due_date').val(undefined);
+                $('#max_points').val(undefined);
+                $('#content').val(undefined);
+            },
+            error: function () {
+                alert('error adding new thing')
+            }});
+    }
+        else {
+        $.ajax({
+            type: 'POST',
+            url: '/edit_assignment',
+            contentType: 'application/json',
+            data: JSON_new_assignment,
+            success: function() {         // response_data = sorted to-do list in JSON format
+                modalAdd.style.display = "none";
+                $('#assignment_title').val(undefined);
+                $("#is_team").prop('checked', false);
+                $('#due_date').val(undefined);
+                $('#max_points').val(undefined);
+                $('#content').val(undefined);
+            },
+            error: function () {
+                alert('error adding new thing')
+            }});
+
+
+        }}
 
 //-------------------------------------------------------------------------------------//
 //                                  MODAL'S REMOVE
@@ -47,57 +159,9 @@ function showModalAdd() {
 //     modalAdd.style.display = "block";
 // }
 
-//-------------------------------------------------------------------------------------//
-//                              MODAL'S ADD ASSIGNMENT
-//-------------------------------------------------------------------------------------//
 
-// add new/edit to-do item (depends on 'choice' parameter: 'new_to_do' / 'edit_to_do'
-function add_new_edit_to_do(choice, id) {
+//------------------------------    TRASH       -----------------------------------------------//
 
-        var modalAdd = document.getElementById('modalAdd');
-
-        if (choice == 'new_assignment') {
-            var path = '/new_assignment';
-        }
-        else {
-            var path = '/edit_assignment';
-        }
-
-        // --------------- get values from form -------------------- //
-        var assignment_title = $('#assignment_title');
-        var is_team = $("#is_team");
-        var due_date = $('#due_date');
-        var max_points = $('#max_points');
-        var content = $('#content');
-
-
-        if (is_team.prop('checked') == false) {
-            is_team = 0}
-        else {is_team = 1}
-
-        var new_assignment = {
-            assignment_title: assignment_title.val(),
-            is_team: is_team,
-            max_points: max_points.val(),
-            due_date: due_date.val(),
-            content: content.val(),
-            id: id
-        };
-        var JSON_new_assignment = JSON.stringify(new_assignment);  // convert JS object to JSON string
-
-        // --------- send values to controller by AJAX: ------------- //
-        $.ajax({
-            type: 'POST',
-            url: '/new_assignment',
-            contentType: 'application/json',
-            data: JSON_new_assignment,
-            success: function() {         // response_data = sorted to-do list in JSON format
-                modalAdd.style.display = "none";
-            },
-            error: function () {
-                alert('error adding new thing')
-            }});
-    }
 
      // success: function(response_data) {         // response_data = sorted to-do list in JSON format
      //            var parsed_response_data = $.parseJSON(response_data); // parse JSON string to JS objects list
