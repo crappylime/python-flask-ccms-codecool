eab1b656ef25d9c1e40bd1459ea0234118beb2a9
Michał Kaczkowski <michal2302@gmail.com>
2017-03-21 11:33:21 +0100

    Add remove_confirm.html template, remove modal functions in common.js

--- static/js/common.js
+++ static/js/common.js
@@ -19,8 +19,30 @@ $(document).ready(function() {
     };
 });
 
-
+    // SHOW MODAL
 function showModalAdd() {
     var modalAdd = document.getElementById('modalAdd');
+    modalAdd.style.display = "block";
+}
+
+
+//-------------------------------------------------------------------------------------//
+//                                  MODAL'S REMOVE
+//-------------------------------------------------------------------------------------//
+
+$(document).ready(function() {
+    var modalAdd = document.getElementById('modalRemove');
+
+    // When the user clicks anywhere outside of the modal, close it //
+    window.onclick = function (event) {
+        if (event.target == modalAdd) {
+            modalAdd.style.display = "none";
+        }
+    };
+});
+
+    // SHOW MODAL
+function showModalRemove() {
+    var modalAdd = document.getElementById('modalRemove');
     modalAdd.style.display = "block";
 }
